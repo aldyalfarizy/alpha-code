94aebb24e7c5ebde046d6be9c050d728bea81840
Aldy <aldy.alfarizy1@gmail.com>
2024-04-25 07:20:52 +0700

    Bismillah

---
+++ src/app/sitemap.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.alpharizy.com",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: "https://www.alpharizy.com/profile",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.3,
+    },
+    {
+      url: "https://www.alpharizy.com/article",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.1,
+    },
+  ];
+}
