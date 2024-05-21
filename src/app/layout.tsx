6116bea30744a6e108119b17505078436ff0f06c
Aldy <aldy.alfarizy1@gmail.com>
2024-05-21 18:36:52 +0700

    minor update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import { cn } from "@/utils/cn";
 
-import "../style/globals.css";
+import "./globals.css";
 
 const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" });
 
